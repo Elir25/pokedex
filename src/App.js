97913ed00c965ba4efa797847711621e0aae980e
Elir25 <elirendiles@gmail.com>
2021-05-13 11:24:22 +0200

    week 3 pokedex until ex E

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import BestPokemon from "./BestPokemon";
 import CaughtPokemon from "./CaughtPokemon";
 import Logo from "./Logo";
+import PokemonCity from "./PokemonCity";
+// import PokemonMoves from "./PokemonMoves";
+import PokemonMovesSelector from "./PokemonMovesSelector"
 
 const abilities = ['Anticipation', 'Adaptability', 'Run-Away', 'Fly'];
 
@@ -18,6 +21,9 @@ function App() {
   <Logo appName="Pokedex" handleClick={logWhenClicked}/>
   <BestPokemon abilities={abilities}/>
   <CaughtPokemon />
+  {/* <PokemonMoves/> */}
+  <PokemonMovesSelector />
+  <PokemonCity />
     </div>
   )
  }
--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 //import { isElement } from "react-dom/cjs/react-dom-test-utils.development";
 
 let date = new Date().toLocaleDateString()
@@ -6,14 +6,19 @@ let date = new Date().toLocaleDateString()
 
 const CaughtPokemon = () => {
     const [caught, setCaught] = useState([])
+    const [pokemonNameInput, setPokemonNameInput] = useState("")
     
     const catchPokemon = () => {
-        //setCaught(caught + 1)
-        setCaught(caught.concat("pikachu"))
+        setCaught(caught + 1)
+        setCaught(caught.concat(pokemonNameInput))
+    }
+    function handlerInputChange(event) {
+        pokemonNameInput(event.target.value)
     }
     return (
         <div>
         <p>Caught {caught.length} Pokemon on {date}</p>
+        <input type="text" value={pokemonNameInput}/>
         <button onClick={catchPokemon}>Catch</button>
         <ul>
             {caught.map((item, index) => {
